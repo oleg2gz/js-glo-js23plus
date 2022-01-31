bdad002d13e5d2ce545ded958e5d07ca8571ef2c
Oleg Kravchenko <kitaist@kitaist.com>
2022-01-31 16:21:33 +0800

    Lesson 3

--- script.js
+++ script.js
@@ -1,22 +1,85 @@
-// 1) Создать переменную num со значением 266219 (тип данных число)
+'use strict'
 
-const num = 266219
+/* 1). Переменная lang может принимать 2 значения: 'ru' 'en'.
+ Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. Решите задачу */
 
-// 2) Вывести в консоль произведение (умножение) цифр этого числа
+const weekdaysObj = {
+  en: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ],
+  ru: [
+    'воскресенье',
+    'понедельник',
+    'вторник',
+    'среда',
+    'четверг',
+    'пятница',
+    'суббота',
+  ],
+}
 
-let result = num
-  .toString()
-  .split('')
-  .reduce((acc, item) => (acc *= item))
+const weekdaysArr = [
+  ['Sunday', 'воскресенье'],
+  ['Monday', 'понедельник'],
+  ['Tuesday', 'вторник'],
+  ['Wednesday', 'среда'],
+  ['Thursday', 'четверг'],
+  ['Friday', 'пятница'],
+  ['Saturday', 'суббота'],
+]
 
-console.log(result)
+const renderWeekdays = (lang, dict = weekdaysObj) => {
+  if (Array.isArray(dict)) {
+    const pos = lang === 'ru' ? 1 : 0
+    weekdaysArr.forEach((day) => console.log(day[pos]))
+  } else if (typeof dict === 'object' && dict != null) {
+    weekdaysObj[lang].forEach((day) => console.log(day))
+  }
+}
 
-// 3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+const lang = 'ru'
+// const lang = 'en'
+// const lang = 'zh'
 
-result **= 3
+// a) через if
+if (lang === 'ru') {
+  renderWeekdays('ru')
+} else if (lang === 'en') {
+  renderWeekdays('en')
+}
 
-// 4) Вывести в консоль первые 2 цифры полученного числа
+// b) через switch-case
+switch (lang) {
+  case 'ru':
+    renderWeekdays('ru')
+    break
+  case 'en':
+    renderWeekdays('en')
+    break
+  default:
+    console.warn('Unknown language')
+}
 
-result = parseInt(result.toString().slice(0, 2))
+// c) через многомерный массив без ифов и switch.
+renderWeekdays(lang, weekdaysArr)
 
-console.log(result)
+/* 2) У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
+ Решить задачу с помощью нескольких тернарных операторов, без использования if или switch. */
+
+const namePerson = 'Артем'
+// const namePerson = 'Александр'
+// const namePerson = 'Олег'
+
+console.log(
+  namePerson === 'Артем'
+    ? 'директор'
+    : namePerson === 'Александр'
+    ? 'преподаватель'
+    : 'студент'
+)
