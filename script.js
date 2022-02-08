c3aedd789118ada882406f425e6c7fff66398f57
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-09 00:59:39 +0800

    Lesson 9

--- script.js
+++ script.js
@@ -1,53 +1,98 @@
 'use strict'
 
-/* Создать массив week и записать в него дни недели в виде строк
-- Вывести на экран все дни недели, каждый с новой строчки
-- Выходные дни - курсивом
-- Текущий день - жирным шрифтом(использовать объект даты)
-*/
+let timers = []
 
-const weekdayStyle = {
-  weekdays: [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ],
-  styles: {
-    bold: 'font-weight: bold;',
-    italic: 'font-style: italic;',
-    boldItalic: 'font-weight: bold; font-style: italic;',
-  },
-  adjustIndex(index) {
-    if (index >= this.weekdays.length) return 0
-    return index
-  },
-  show() {
-    this.weekdays.forEach((day, i) => {
-      if (
-        i === this.adjustIndex(new Date().getDate() + 1) &&
-        (i === 0 || i === 6)
-      ) {
-        this.styleDay(day, 'boldItalic')
-      } else if (i === 0 || i === 6) {
-        this.styleDay(day, 'italic')
-      } else if (i === this.adjustIndex(new Date().getDate() + 1)) {
-        this.styleDay(day, 'bold')
-      } else {
-        this.styleDay(day)
-      }
+const formatDate = (format) => {
+  const date = new Date()
+  const dateString = date
+    .toLocaleString('ru-Ru', {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
     })
-  },
-  styleDay(day, style) {
-    document.body.insertAdjacentHTML(
-      'beforeend',
-      `<p style="${this.styles[style]}">${day}</p>`
-    )
-    console.log('%c' + day, this.styles[style])
-  },
+    .replace('г.', 'года')
+  const dateNumeric = date.toLocaleString('ru-Ru', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+  })
+  const dateWatch = date.toLocaleString('ru-Ru', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  })
+
+  // Окончания для "час, минута, секунда"
+
+  const adjustEnding = (str, type) => {
+    const last = +str[str.length - 1]
+
+    if (
+      (last === 2 && str != 12) ||
+      (last === 3 && str != 13) ||
+      (last === 4 && str != 14)
+    ) {
+      if (type === 'hour') return 'а'
+      else return 'ы'
+    } else if (last === 1 && str != 11) {
+      if (type === 'hour') return ''
+      else return 'а'
+    } else {
+      if (type === 'hour') return 'ов'
+      else return ''
+    }
+  }
+
+  // Время в формате "21 час 5 минут 33 секунды"
+
+  const hrMinSec = dateWatch
+    .split(':')
+    .map((item, i) => {
+      if (i === 0) return `${item} час${adjustEnding(item, 'hour')}`
+      if (i === 1) return `${item} минут${adjustEnding(item)}`
+      if (i === 2) return `${item} секунд${adjustEnding(item)}`
+    })
+    .join(' ')
+    .trim()
+
+  return format === 'full'
+    ? `Сегодня ${
+        dateString[0].toUpperCase() + dateString.slice(1)
+      }, ${hrMinSec}`
+    : `${dateNumeric} - ${dateWatch}`
 }
 
-weekdayStyle.show()
+// Создание разметки для вывода на странице
+
+const renderMarkup = () => {
+  for (let i = 0; i < 2; i++) {
+    const p = document.createElement('p')
+
+    if (i === 0) {
+      p.textContent = formatDate('full')
+    } else if (i === 1) {
+      p.textContent = formatDate()
+    }
+    timers.push(p)
+    document.body.append(p)
+  }
+}
+
+renderMarkup()
+
+setInterval(() => {
+  timers[0].textContent = formatDate('full')
+  timers[1].textContent = formatDate()
+}, 1000)
+
+// Функция, которая добавляет 0 перед значениями, состоящими из одной цифры
+// (из 9:5:3 1.6.2019 сделает 09:05:03 01.06.2019)
+
+const adjustNumber = (str) => {
+  if (str.length < 2) return 0 + str
+  return str
+}
+
+console.log(adjustNumber('21'))
+console.log(adjustNumber('9'))
