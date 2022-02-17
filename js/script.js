af026a948b4c2f55240690c6ae82e451a9f0df24
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-17 14:44:35 +0800

    Lesson 15, all ready

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 'use strict'
 
+let squareMaker = null
+let square = null
+let styles = null
+
 const DomElement = function (selector, height, width, bg, fontSize) {
   this.selector = selector
-  this.height = height
-  this.width = width
+  this.height = height + 'px'
+  this.width = width + 'px'
   this.bg = bg
-  this.fontSize = fontSize
+  this.fontSize = fontSize + 'px'
 }
 
 DomElement.prototype.createElement = function () {
@@ -18,29 +22,38 @@ DomElement.prototype.createElement = function () {
     elem = document.createElement('p')
     elem.id = this.selector.slice(1)
   }
-  elem.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; fontSize: ${this.fontSize}px;`
+  elem.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`
   elem.textContent = 'MIROMAX!'
   document.body.append(elem)
+
+  return elem
 }
 
-const squareMaker = new DomElement('.square', 150, 150, 'red', 18)
-squareMaker.createElement()
+squareMaker = new DomElement('.square', 100, 100, 'red', 18)
+square = squareMaker.createElement()
+square.style.position = 'absolute'
+styles = getComputedStyle(square)
+square.style.top = styles.top
+square.style.left = styles.left
 
-// пример:
-// если передана строка '.block', то функция конструктор создает элемент с class="block"
+document.addEventListener('keydown', (e) => {
+  const top = parseFloat(square.style.top)
+  const left = parseFloat(square.style.left)
 
-// если передана строка '#best', то функция конструктор создает элемент с id =best"
+  switch (e.key) {
+    case 'ArrowLeft':
+      square.style.left = left - 10 + 'px'
+      break
 
-// Внутрь созданного блока записывать любой текст. Метод записи может быть любым.
+    case 'ArrowRight':
+      square.style.left = left + 10 + 'px'
+      break
 
-// 2) Создать новый объект на основе класса DomElement
+    case 'ArrowUp':
+      square.style.top = top - 10 + 'px'
+      break
 
-// 3) Вызвать его метод чтобы создать элемент на странице
-
-// УСЛОЖНЕННОЕ  ЗАДАНИЕ: 1 БАЛЛ
-
-// 1) Используя функцию-конструктор DomElement из основного задания №1, создать квадрат 100 на 100 пикселей. Ему необходимо задать фон(background) любого цвета и свойство position: absolute.
-
-// 2) Поместить его на страницу только после выполнения события DOMContentLoaded. Внутри тега body должно быть только подключение скрипта. (В случае подключения файла скрипта к странице перед закрывающим тэгом body)
-
-// 3) Написать обработчик события для keydown, который будет принимать callback-функцию. Данная функция будет отлавливать нажатие на стрелки клавиатуры. В зависимости от нажатой кнопки(Вверх - стрелка вверх, Влево - стрелка влево, Вправо - стрелка вправо, Вниз - стрелка вниз) наш квадрат будет перемещаться на 10 пикселей при каждом нажатии.
+    case 'ArrowDown':
+      square.style.top = top + 10 + 'px'
+  }
+})
