db125c0e60c77ef90b4e2c6ecc559537668497bb
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-19 01:20:43 +0800

    Lesson 16

--- js/script.js
+++ js/script.js
@@ -1,59 +1,20 @@
-'use strict'
-
-let squareMaker = null
-let square = null
-let styles = null
-
-const DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = selector
-  this.height = height + 'px'
-  this.width = width + 'px'
-  this.bg = bg
-  this.fontSize = fontSize + 'px'
+class First {
+  hello() {
+    console.log('Привет я метод родителя!')
+  }
 }
 
-DomElement.prototype.createElement = function () {
-  let elem = null
-
-  if (this.selector[0] === '.') {
-    elem = document.createElement('div')
-    elem.classList.add(this.selector.slice(1))
-  } else if (this.selector[0] === '#') {
-    elem = document.createElement('p')
-    elem.id = this.selector.slice(1)
+class Second extends First {
+  hello() {
+    super.hello()
+    console.log('А я наследуемый метод!')
   }
-  elem.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`
-  elem.textContent = 'MIROMAX!'
-  document.body.append(elem)
-
-  return elem
 }
 
-squareMaker = new DomElement('.square', 100, 100, 'red', 18)
-square = squareMaker.createElement()
-square.style.position = 'absolute'
-styles = getComputedStyle(square)
-square.style.top = styles.top
-square.style.left = styles.left
+const first = new First()
+const second = new Second()
 
-document.addEventListener('keydown', (e) => {
-  const top = parseFloat(square.style.top)
-  const left = parseFloat(square.style.left)
-
-  switch (e.key) {
-    case 'ArrowLeft':
-      square.style.left = left - 10 + 'px'
-      break
-
-    case 'ArrowRight':
-      square.style.left = left + 10 + 'px'
-      break
-
-    case 'ArrowUp':
-      square.style.top = top - 10 + 'px'
-      break
-
-    case 'ArrowDown':
-      square.style.top = top + 10 + 'px'
-  }
-})
+console.log('Run for first:')
+first.hello()
+console.log('Run for second:')
+second.hello()
