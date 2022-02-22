19f11e8600b83842c0bcf00648dd675304bc02b2
Oleg Kravchenko <kitaist@kitaist.com>
2022-02-23 05:02:34 +0800

    Lesson 19A, initial commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,51 @@
-'use strict'
+const userInput = document.getElementById('user-input')
+const inputText = document.getElementById('input-text')
+let inputTimeout
 
-class First {
-  hello() {
-    console.log('Привет я метод родителя!')
+userInput.addEventListener('input', () => {
+  clearTimeout(inputTimeout)
+
+  inputTimeout = setTimeout(() => {
+    inputText.textContent = userInput.value
+  }, 300)
+})
+
+const cube = document.querySelector('.cube')
+const btnMove = document.querySelector('.move')
+const btnReset = document.querySelector('.reset')
+const cubeField = document.querySelector('.cube-field')
+const cubeFieldWidthValue = parseFloat(getComputedStyle(cubeField).width)
+let posX = 0
+let animation
+
+const animateCube = () => {
+  if (posX >= cubeFieldWidthValue) {
+    cancelAnimationFrame(animation)
+    cube.classList.add('circle')
+    btnMove.textContent = 'move'
+    return
   }
+  animation = requestAnimationFrame(animateCube)
+  cube.style.left = posX++ + 'px'
 }
 
-class Second extends First {
-  hello() {
-    super.hello()
-    console.log('А я наследуемый метод!')
+const controlMove = () => {
+  if (btnMove.textContent === 'pause') {
+    cancelAnimationFrame(animation)
+    btnMove.textContent = 'move'
+    return
   }
+  btnMove.textContent = 'pause'
+  animateCube()
 }
 
-const first = new First()
-const second = new Second()
+const controlReset = () => {
+  cancelAnimationFrame(animation)
+  cube.classList.remove('circle')
+  btnMove.textContent = 'move'
+  cube.style.left = ''
+  posX = 0
+}
 
-console.log('Run for first:')
-first.hello()
-console.log('Run for second:')
-second.hello()
+btnMove.addEventListener('click', controlMove)
+btnReset.addEventListener('click', controlReset)
